e3575f8825b572e9ad7493876ea2917e866e0d37
Dave Holst <daveholst@gmail.com>
2023-01-22 14:30:17 +0800

    OTA flashing working with new 8ch board

--- arduino-rest-server/src/main.cpp
+++ arduino-rest-server/src/main.cpp
@@ -1,13 +1,8 @@
 #include "secrets.h"
-#include <WiFi.h>
-#include <ESPmDNS.h>
-
-// #include <WebServer.h>
-// #include <ArduinoJson.h>
-// #include <WebSocketsClient.h>
-// #include <ArduinoWebsockets.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
 #include <PubSubClient.h>
-#include <HX711.h>
+// #include <HX711.h>
 #include <ArduinoOTA.h>
 
 const char *SSID = SECRET_SSID;
@@ -16,9 +11,9 @@ const char *MQTTUSR = SECRET_MQTTUSR;
 const char *MQTTPWD = SECRET_MQTTPWD;
 const char *MQTTSERVER = SECRET_MQTTSERVER;
 // Device Identifier
-const char *DEVICE = "juicebox_1";
+const char *DEVICE = "juicebox_2";
 // device hostname
-String hostname = "juicebox_1";
+String hostname = "juicebox_2";
 
 // Led Pins
 const int statusLed = 2;
@@ -29,25 +24,23 @@ const int relay1 = 27;
 const int relay2 = 26;
 const int relay3 = 25;
 // Load Cell Pins
-const int loadcellDout = 23;
-const int loadcellSck = 22;
+// const int loadcellDout = 23;
+// const int loadcellSck = 22;
 // mqtt payloads
 String weight_str;
 char weight[50];
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
-HX711 scale;
+// HX711 scale;
 // long lastMsg = 0;
 // char msg[50];
 // int value = 0;
 
-
-
-
 void connectToWiFi()
 {
-  WiFi.setHostname(hostname.c_str());;
+  WiFi.setHostname(hostname.c_str());
+  ;
   Serial.print("Connecting to ");
   Serial.println(SSID);
 
@@ -78,7 +71,7 @@ void callback(char *topic, byte *message, unsigned int length)
   }
   Serial.println();
 
-  if (String(topic) == "juicebox1/relay1")
+  if (String(topic) == "juicebox2/relay1")
   {
     Serial.print("Changing Relay 1 output to ");
     if (messageIn == "on")
@@ -92,7 +85,7 @@ void callback(char *topic, byte *message, unsigned int length)
       digitalWrite(relay1, HIGH);
     }
   }
-  if (String(topic) == "juicebox1/relay2")
+  if (String(topic) == "juicebox2/relay2")
   {
     Serial.print("Changing Relay 2 output to ");
     if (messageIn == "on")
@@ -106,7 +99,7 @@ void callback(char *topic, byte *message, unsigned int length)
       digitalWrite(relay2, HIGH);
     }
   }
-    if (String(topic) == "juicebox1/relay3")
+  if (String(topic) == "juicebox2/relay3")
   {
     Serial.print("Changing Relay 3 output to ");
     if (messageIn == "on")
@@ -132,11 +125,11 @@ void reconnect()
     {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      mqttClient.publish("juicebox1", "hello world");
+      mqttClient.publish("juicebox2", "hello world");
       // ... and resubscribe
-      mqttClient.subscribe("juicebox1/relay1");
-      mqttClient.subscribe("juicebox1/relay2");
-      mqttClient.subscribe("juicebox1/relay3");
+      mqttClient.subscribe("juicebox2/relay1");
+      mqttClient.subscribe("juicebox2/relay2");
+      mqttClient.subscribe("juicebox2/relay3");
       // mqttClient.subscribe("juicebox1/relay2");
     }
     else
@@ -150,24 +143,27 @@ void reconnect()
   }
 }
 
-void printScales()
-{
-  if (scale.wait_ready_timeout(1000)) {
-    // long reading = scale.read();
-    long reading = scale.get_units(15);
-    String readingTempString = String(reading);
-    char readingMessage[50];
-    readingTempString.toCharArray(readingMessage, readingTempString.length() + 1);
-    // String messageToSend = String("{\"tankWeight\": \" "readingString" \" }");
-    mqttClient.publish("juicebox1/tankWeight", readingMessage);
-    Serial.print("HX711 reading: ");
-    Serial.println(reading);
-  } else {
-    Serial.println("HX711 not found :(");
-  }
+// void printScales()
+// {
+//   if (scale.wait_ready_timeout(1000))
+//   {
+//     // long reading = scale.read();
+//     long reading = scale.get_units(15);
+//     String readingTempString = String(reading);
+//     char readingMessage[50];
+//     readingTempString.toCharArray(readingMessage, readingTempString.length() + 1);
+//     // String messageToSend = String("{\"tankWeight\": \" "readingString" \" }");
+//     mqttClient.publish("juicebox2/tankWeight", readingMessage);
+//     Serial.print("HX711 reading: ");
+//     Serial.println(reading);
+//   }
+//   else
+//   {
+//     Serial.println("HX711 not found :(");
+//   }
 
-  // delay(1500);
-}
+//   // delay(1500);
+// }
 
 void setup()
 {
@@ -179,7 +175,7 @@ void setup()
   digitalWrite(relay2, HIGH); // !start relay1 HIGH (Which is off????)
   pinMode(relay3, OUTPUT);
   digitalWrite(relay3, HIGH); // !start relay1 HIGH (Which is off????)
-  //leds
+  // leds
   pinMode(greenLed, OUTPUT);
   pinMode(blueLed, OUTPUT);
   Serial.begin(9600);
@@ -190,40 +186,28 @@ void setup()
   mqttClient.setCallback(callback);
 
   // start scales
-  scale.begin(loadcellDout, loadcellSck);
+  // scale.begin(loadcellDout, loadcellSck);
   // calibration
-  scale.set_scale(-22.3);
-  scale.set_offset(-576184);
+  // scale.set_scale(-22.3);
+  // scale.set_offset(-576184);
   // scale.tare();
 
   // OTA Setup
-  ArduinoOTA.setHostname("juicebox_1");
-  ArduinoOTA
-    .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
-
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
-
+  ArduinoOTA.setHostname("juicebox_2");
+  ArduinoOTA.onStart([]()
+                     { Serial.println("Start"); });
+  ArduinoOTA.onEnd([]()
+                   { Serial.println("\nEnd"); });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+                        { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
+  ArduinoOTA.onError([](ota_error_t error)
+                     {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
   ArduinoOTA.begin();
 
   Serial.println("Ready");
@@ -237,8 +221,8 @@ void loop()
   {
     reconnect();
   }
-  printScales();
+  // printScales();
   ArduinoOTA.handle();
   mqttClient.loop();
-     //not using - will try again later!
+  // not using - will try again later!
 }
